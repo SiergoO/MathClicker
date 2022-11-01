5644c01effb99f8d5307c618980b2f2a74030de4
sdomashchuk <Sergey.Domashchuk@softeq.com>
2022-11-01 14:50:04 +0200

    Create blank app from compose template. Create app modules. Setup gradle.kts files.

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.6.10" apply false
+    id("com.android.application") version "7.3.1" apply false
+    id("com.android.library") version "7.3.1" apply false
+}
+
+buildscript {
+    repositories {
+        mavenCentral {
+            metadataSources {
+                mavenPom()
+                artifact()
+                ignoreGradleMetadataRedirection()
+            }
+        }
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.41")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
